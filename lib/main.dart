9f969accff2fd89263b1b5e63d9f58f04a00e02c
ahmedelne7 <ahmedelnahrawy881@gmail.com>
2025-01-30 21:29:10 +0200

    adding firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_shopping_app/views/sign_up_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   runApp(
@@ -10,7 +16,8 @@ void main() async {
     EasyLocalization(
       supportedLocales: const [Locale('en', 'US'), Locale('ar', 'EG')],
       path: 'lang',
-      fallbackLocale: const Locale('en', 'US'), // Setting English as a defult language
+      fallbackLocale:
+          const Locale('en', 'US'), // Setting English as a defult language
       child: const SimpleShoppingApp(),
     ),
   );
